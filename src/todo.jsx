89074784724e305aa7453ed7ec9d402591a5b957
Suharika P <suharikap@gmail.com>
2020-06-02 10:39:39 +0530

    Initial setup

---
+++ src/todo.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Todo extends React.Component{
+    state= {
+        items :[],
+        text : ''
+    };
+  submitButton(){
+      const text = this.state.text;
+      this.setState = this.state.items.concat(text)
+  }
+    render(){
+        return(
+            <div>
+                <h4>TODO</h4>
+                <form onSubmit={this.submitButton}>
+                <input type='textbox' value={this.state.text} ></input>
+                <br/>
+                <button type='submit'>Add</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Todo;
